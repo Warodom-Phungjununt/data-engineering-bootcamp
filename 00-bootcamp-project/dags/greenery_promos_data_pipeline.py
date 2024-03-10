300eb1e5d8a79540ecded990e0edcf8103f16caa
Warodom-Phungjununt <155611313+Warodom-Phungjununt@users.noreply.github.com>
2024-03-10 10:28:09 +0000

    Add solution for Data Pipeline Class

---
+++ 00-bootcamp-project/dags/greenery_promos_data_pipeline.py
@@ -0,0 +1,134 @@
+import csv
+import json
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils import timezone
+
+import requests
+from google.cloud import bigquery, storage
+from google.oauth2 import service_account
+
+
+BUSINESS_DOMAIN = "greenery"
+LOCATION = "asia-southeast1"
+PROJECT_ID = "my-deb-workshop"
+DAGS_FOLDER = "/opt/airflow/dags"
+DATA = "promos"
+
+
+def _extract_data():
+    url = f"http://34.87.139.82:8000/{DATA}/"
+    response = requests.get(url)
+    data = response.json()
+
+    with open(f"{DAGS_FOLDER}/{DATA}.csv", "w") as f:
+        writer = csv.writer(f)
+        header = [
+            "promo_id",
+            "discount",
+            "status",
+        ]
+        writer.writerow(header)
+        for each in data:
+            data = [
+                each["promo_id"],
+                each["discount"],
+                each["status"],
+            ]
+            writer.writerow(data)
+
+
+def _load_data_to_gcs(ds):
+    keyfile_gcs = "/opt/airflow/dags/my-deb-workshop-deb3-uploading-files-to-gcs-18004ff4f153.json"
+    service_account_info_gcs = json.load(open(keyfile_gcs))
+    credentials_gcs = service_account.Credentials.from_service_account_info(
+        service_account_info_gcs
+    )
+
+    # Load data from Local to GCS
+    bucket_name = "deb3-bootcamp-23"
+    storage_client = storage.Client(
+        project=PROJECT_ID,
+        credentials=credentials_gcs,
+    )
+    bucket = storage_client.bucket(bucket_name)
+
+    file_path = f"{DAGS_FOLDER}/{DATA}.csv"
+    destination_blob_name = f"{BUSINESS_DOMAIN}/{DATA}/{DATA}.csv"
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(file_path)
+
+
+def _load_data_from_gcs_to_bigquery():
+    keyfile_bigquery = "/opt/airflow/dags/my-deb-workshop-deb3-load-files-to-bigquery-65990ca3a02e.json"
+    service_account_info_bigquery = json.load(open(keyfile_bigquery))
+    credentials_bigquery = service_account.Credentials.from_service_account_info(
+        service_account_info_bigquery
+    )
+
+    bigquery_client = bigquery.Client(
+        project=PROJECT_ID,
+        credentials=credentials_bigquery,
+        location=LOCATION,
+    )
+
+    table_id = f"{PROJECT_ID}.deb_bootcamp.{DATA}"
+    job_config = bigquery.LoadJobConfig(
+        skip_leading_rows=1,
+        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+        source_format=bigquery.SourceFormat.CSV,
+        autodetect=True,
+    )
+
+    bucket_name = "deb3-bootcamp-23"
+    destination_blob_name = f"{BUSINESS_DOMAIN}/{DATA}/{DATA}.csv"
+    job = bigquery_client.load_table_from_uri(
+        f"gs://{bucket_name}/{destination_blob_name}",
+        table_id,
+        job_config=job_config,
+        location=LOCATION,
+    )
+    job.result()
+
+    table = bigquery_client.get_table(table_id)
+    print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": timezone.datetime(2021, 2, 9),
+}
+with DAG(
+    dag_id="greenery_promos_data_pipeline",
+    default_args=default_args,
+    schedule="@daily",
+    catchup=False,
+    tags=["DEB", "Skooldio", "greenery"],
+):
+
+    # Extract data from Postgres, API, or SFTP
+    extract_data = PythonOperator(
+        task_id="extract_data",
+        python_callable=_extract_data,
+        op_kwargs={"ds": "{{ ds }}"},
+    )
+
+    # Load data to GCS
+    load_data_to_gcs = PythonOperator(
+        task_id="load_data_to_gcs",
+        python_callable=_load_data_to_gcs,
+        op_kwargs={"ds": "{{ ds }}"},
+    )
+
+    # Load data from GCS to BigQuery
+    load_data_from_gcs_to_bigquery = PythonOperator(
+        task_id="load_data_from_gcs_to_bigquery",
+        python_callable=_load_data_from_gcs_to_bigquery,
+        op_kwargs={"ds": "{{ ds }}"},
+    )
+
+    # Task dependencies
+    extract_data >> load_data_to_gcs >> load_data_from_gcs_to_bigquery
